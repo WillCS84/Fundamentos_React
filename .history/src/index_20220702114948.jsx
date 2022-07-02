f95760773d65cb58ea03a8bb03db4f957e010f96
Willian Santos <will.cesar.84@gmail.com>
2022-07-02 13:25:46 -0300

    ignore

--- .history/src/index_20220702114948.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import { render } from "react-dom";
-import { App } from "./app"
-
-render(<h1>Test</h1>, document.getElementById("root"));
--- .history/src/index_20220702115458.jsx
+++
@@ -1,4 +0,0 @@
-import { render } from "react-dom";
-import { App } from "./app"
-
-render(<h1>Test</h1>, document.getElementById("root"));
